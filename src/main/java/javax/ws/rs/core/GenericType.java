ca61da400def665972f3e89ef905c43b21890df4
Donald Jackson <donald.jackson@node.co.za>
2021-09-20 10:08:31 +0200

    same constructors

--- src/main/java/javax/ws/rs/core/GenericType.java
+++ src/main/java/javax/ws/rs/core/GenericType.java
@@ -1,4 +1,12 @@
 package javax.ws.rs.core;
 
+import java.lang.reflect.Type;
+
 public class GenericType<T> extends com.sun.jersey.api.client.GenericType<T> {
+    public GenericType() {
+        super();
+    }
+    public GenericType(Type genericType) {
+        super(genericType);
+    }
 }
